8564740cb3145bf7be21b826a79baa2d1bbc9ed7
ChanoHL <shanohl.sist@gmail.com>
2019-01-10 18:48:36 -0500

    Change url keycloack

--- src/config-local/openfact-ui.env.js
+++ src/config-local/openfact-ui.env.js
@@ -1,5 +1,5 @@
 window.KeycloakUIEnv = {
-  "ssoRealm": "ahren",
-  "ssoApiUrl": "http://localhost:8080/auth",
+  "ssoRealm": "innpath",
+  "ssoApiUrl": "https://sso-sso.b9ad.pro-us-east-1.openshiftapps.com/auth",//"http://localhost:8080/auth",
   "ssoClientID": "openfact-web-console"
 };
