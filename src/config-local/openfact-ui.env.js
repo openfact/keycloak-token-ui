ad6e8cbb6825a2fec3a697ddaaa5f980ed4c06a3
Alex Pariona <lx7pary@gmail.com>
2019-01-11 10:50:16 -0500

    ...

--- src/config-local/openfact-ui.env.js
+++ src/config-local/openfact-ui.env.js
@@ -1,8 +1,8 @@
 
 window.KeycloakUIEnv = {
-  "ssoRealm":  process.env.SSO_REALM ,
-  "ssoApiUrl": process.env.SSO_API_URL ,
-  "ssoClientID": process.env.SSO_CLIENT_ID
+  // "ssoRealm":  process.env.SSO_REALM ,
+  // "ssoApiUrl": process.env.SSO_API_URL ,
+  // "ssoClientID": process.env.SSO_CLIENT_ID
  /* "ssoRealm": "innpath",
   "ssoApiUrl": "https://sso-sso.b9ad.pro-us-east-1.openshiftapps.com/auth",//"http://localhost:8080/auth",
   "ssoClientID": "openfact-web-console"*/
