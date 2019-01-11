1bba54f5a0fe0914027cc9e934f049f3c06f74a2
ChanoHL <shanohl.sist@gmail.com>
2019-01-11 08:49:13 -0500

    fix config local

--- src/config-local/openfact-ui.env.js
+++ src/config-local/openfact-ui.env.js
@@ -1,7 +1,7 @@
 window.KeycloakUIEnv = {
-  "ssoRealm":  process.env.Env.SSO_REALM ,
-  "ssoApiUrl": process.env.Env.SSO_API_URL ,
-  "ssoClientID": process.env.Env.SSO_CLIENT_ID
+  "ssoRealm":  process.env.SSO_REALM ,
+  "ssoApiUrl": process.env.SSO_API_URL ,
+  "ssoClientID": process.env.SSO_CLIENT_ID
  /* "ssoRealm": "innpath",
   "ssoApiUrl": "https://sso-sso.b9ad.pro-us-east-1.openshiftapps.com/auth",//"http://localhost:8080/auth",
   "ssoClientID": "openfact-web-console"*/
